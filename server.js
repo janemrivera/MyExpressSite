fe466d49e96507db6d1975daf89278eb57da632d
janemrivera <jane.m.rivera@gmail.com>
2017-01-19 10:33:06 -0600

    Added Flint

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+'use strict';
+
+var Flint = require('node-flint');
+var webhook = require('node-flint/webhook');
+var config = {
+  "webhookUrl": "https://testexpresssite.azurewebsites.net",
+  "token": "ZjQ5YTNjZDUtMWQxOC00YzkzLWIxODEtNDM1OTAzNjU2MjVkNzg5M2M4ZTItYWIw",
+  "port": "8080"
+}
+
 var express = require('express');
 var app = express();
 
@@ -7,6 +17,44 @@ var port = process.env.port || 8080;
 //Serve up files in public folder
 app.use('/', express.static(__dirname + '/public'));
 
+// init flint
+var flint = new Flint(config);
+flint.start();
+console.log("Starting flint, please wait...");
+
+flint.on("initialized", function() {
+  console.log("Flint initialized successfully! [Press CTRL-C to quit]");
+});
+
 //Start up the website
-app.listen(port);
+var server = app.listen(port);
 console.log('Listening on port: ', port);
+
+// gracefully shutdown (ctrl-c)
+process.on('SIGINT', function() {
+  flint.debug('stopping...');
+  server.close();
+
+  flint.stop().then(function() {
+    process.exit();
+  });
+
+});
+
+/*
+
+{
+	"id": "Y2lzY29zcGFyazovL3VzL1dFQkhPT0svYjY3YzAwNTUtZTBhYS00Y2JmLWJjNDctMGI0YTJhNGU4NGVj",
+	"name": "MyExpressWebhook",
+	"targetUrl": "https://testexpresssite.azurewebsites.net",
+	"resource": "all",
+	"event": "all",
+	"orgId": "Y2lzY29zcGFyazovL3VzL09SR0FOSVpBVElPTi9jZjlhNzMyMC0xZjM3LTQyMGQtYWI0MS1lOGM3Zjg0NjM1NzI",
+	"createdBy": "Y2lzY29zcGFyazovL3VzL1BFT1BMRS80NzUwMTcwYi1hYzE0LTQ1OTgtOTc4MC03YTFhZTBhNDAxMmU",
+	"appId": "Y2lzY29zcGFyazovL3VzL0FQUExJQ0FUSU9OL0MzMmM4MDc3NDBjNmU3ZGYxMWRhZjE2ZjIyOGRmNjI4YmJjYTQ5YmE1MmZlY2JiMmM3ZDUxNWNiNGEwY2M5MWFh",
+	"ownedBy": "creator",
+	"status": "active",
+	"created": "2017-01-19T16:04:04.241Z"
+}
+
+*/
