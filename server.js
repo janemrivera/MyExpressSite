b9f8c43b7b3a7cc75854e43be4b78b18619962a2
janemrivera <jane.m.rivera@gmail.com>
2017-01-20 10:13:44 -0600

    add all test commands

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var Flint = require('node-flint');
 var webhook = require('node-flint/webhook');
 var config = {
-  "webhookUrl": "https://testexpresssite.azurewebsites.net",
+  "webhookUrl": "https://testexpresssite.azurewebsites.net", // "https://ferb.localtunnel.me",
   "token": "ZjQ5YTNjZDUtMWQxOC00YzkzLWIxODEtNDM1OTAzNjU2MjVkNzg5M2M4ZTItYWIw",
   "port": 8080
 }
@@ -103,7 +103,7 @@ flint.hears('/help', function(bot, trigger) {
 
 flint.hears('/about', function(bot, trigger) {
   console.log("/about fired");
-  let outputString = "```\n{\n  'author':'Jane Rivera &lt;jmrivera@nalco.com&gt;',\n  'code':'helloworld.js on local',\n  'description':'a test bot for checking out the Spark APIs',\n  'healthcheck':'GET https://www.test.com',\n  'webhook':'POST https://www.test.com'\n}\n```"
+  let outputString = "```\n{\n  'author':'Jane Rivera <jmrivera@nalco.com>;',\n  'code':'My Express App with Ferb',\n  'description':'S test bot for checking out the Spark APIs',\n  'healthcheck':'GET https://testexpresssite.azurewebsites.net/',\n  'webhook':'POST https://testexpresssite.azurewebsites.net/'\n}\n```"
    bot.say("markdown", outputString);
 });
 
@@ -127,8 +127,35 @@ flint.hears('/whoami', function(bot, trigger) {
   let roomTitle = "**" + trigger.roomTitle + "**";
   let personEmail = trigger.personEmail;
   let personDisplayName = trigger.personDisplayName;
-  let outputString = `${personDisplayName} here is some of your information: \n\n\n **Room:** you are in &ldquo;${roomTitle}&rdquo; \n\n\n **Room id:** ${roomId} \n\n\n **Email:** your email on file is *${personEmail}*`;
+  let outputString = `${personDisplayName} here is some of your information: \n\n\n **Room:** you are in "${roomTitle}" \n\n\n **Room id:** ${roomId} \n\n\n **Email:** your email on file is *${personEmail}*`;
   bot.say("markdown", outputString);
+});
+
+flint.hears('/whois',function(bot, trigger){
+  console.log("/whois fired");
+  //console.log(trigger.mentionedPeople);
+  if(trigger.mentionedPeople.length != 2)
+  {
+      bot.say("Sorry, I cannot proceed if you do not mention a room participant.")
+      return;
+  }
+
+  var participant = trigger.mentionedPeople[1] ;
+  //console.log(participant);
+  try{
+    //getPersonById has been added by Jane
+    flint.getPersonById(participant).then(function(person){
+      let personId = person.id;
+      let displayName = person.displayName;
+      let personEmail = person.email;
+      let outputString = `Person Id: ${personId} \n\n Display Name: ${displayName} \n\n Email: ${personEmail}`;
+      bot.say("markdown", outputString)
+    });
+  }
+  catch(err){
+      bot.say("Unable to get the person information. Usage: @botMention /whois @personMention")
+  }
+
 });
 
 /* On mention with command arguments
@@ -172,7 +199,9 @@ flint.hears(/(^| )beer( |.|$)/i, function(bot, trigger, id) {
   bot.say('Enjoy a beer, %s! ðŸ»', trigger.personDisplayName);
 });
 
-//weather APIs
+/****************************************
+## Weather API
+****************************************/
 flint.hears('/weather', function(bot, trigger) {
   // /(^| )weather( |.|$)/i
   //bot.say('Enjoy a beer, %s! ðŸ»', trigger.personDisplayName);
@@ -261,7 +290,313 @@ flint.hears('/weather', function(bot, trigger) {
 });
 
 
+/****************************************
+## Meme Generator
+****************************************/
+var meme = {
 
+  brace: {id: "61546",desc: "** - Brace yourselves, ... is coming"},
+	conspiracy: {id: "61583",desc: "** - Keanu Reeves conspiracy :frowning:"},
+	fry: {id: "61520",desc: "** - Not sure if ... or ... :confused:"},
+	onedoesnot: {id: "61579",desc: "** - One does not simply"},
+	yuno: {id: "61527",desc: "** - Y U No"},
+	success: {id: "61544",desc: "** - Success kid"},
+	allthethings: {id: "61533",desc: "** - All the things"},
+	doge: {id: "8072285",desc: "** - Such wow Much meme"},
+	drevil: {id: "40945639",desc: "** - Dr Evil quotes"},
+	skeptical: {id: "101711",desc: "** - Skeptical kid"},
+	idontalways: {id: "61532",desc: "** - I don't always ... but when I do ..."},
+	yodawg: {id: "101716",desc: "** - Yo Dawg"},
+	joker: {id: "1790995",desc: "** - And everybody loses their minds"}
+
+}
+
+flint.hears('/meme', function(bot, trigger) {
+  console.log("/meme fired");
+
+  var bot_text = trigger.args.slice(1).join(" ");
+  //bot.say(bot_text);
+
+  var tags = bot_text.split('"'); //var tags = msg.content.split('"');
+
+  var memetype = tags[0].trim();
+  //var memetype = tags[0];
+  //console.log(memetype);
+
+  var memeValues = meme[memetype]; //meme['yuno'];
+  //console.log(memeValues);
+  //console.log(memeValues.id);
+  var memeId = memeValues.id;
+
+  //console.log(memeId);
+
+  var top_text = tags[1]?tags[1]:"";
+  var bottom_text = tags[3]?tags[3]:"";
+
+  //bot.say("Meme ID is %s - %s, %s", memeId, top_text, bottom_text);
+
+  var imgflipper = new Imgflipper("theeya","theeya");
+  imgflipper.generateMeme(memeId, top_text, bottom_text, function(err, url) {
+    if(!err)
+    {
+      bot.say({text: '', file: url});
+    }
+    else{
+      bot.say('Invalid parameters for command - %s', err);
+    }
+  });
+
+});
+
+flint.hears('/memehelp', function(bot, trigger) {
+  console.log("/memehelp fired");
+  let outputString = 'Usage: **/meme memetype &ldquo;top text&rdquo; &ldquo;bottom text&rdquo;** \n\n Currently available memes:\n';
+  for (var m in meme){
+      outputString += "\t\t" + m + " " + meme[m].desc + "\n"
+  }
+
+  bot.say('markdown', outputString);
+});
+
+
+/****************************************
+ ServiceNow Test
+****************************************/
+flint.hears('/sn-checkstatus', function(bot, trigger) {
+  console.log("/sn-checkstatus fired");
+  var ticketNo = trigger.args.slice(1).join(" ");
+
+ //TODO: creation of token when it expires
+
+
+  var options = { method: 'GET',
+    url: 'https://ecolabsandbox.service-now.com/api/now/table/incident',
+    qs: { sysparm_limit: '1',
+          number: ticketNo,
+          sysparm_fields: 'number,sys_id,state,sys_created_by,short_description,priority,active,assignment_group,assigned_to,caller_id'
+        },
+    headers:
+     {
+       'cache-control': 'no-cache',
+       authorization: 'Bearer LqYAXBdz9pqqPbAKFBhAZpy0g99I0kkmz5I0NJY0czCPDdX0-GrzLYeF9SdAUR46uHL-L64TXkPRlMs8skV7LQ',
+       'content-type': 'application/json',
+       accept: 'application/json' }
+   };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    var jsonbody = JSON.parse(body);
+    var sndata = jsonbody.result[0];
+    //console.log(jsonbody);
+
+    var snNmber = sndata.number;
+    var snDescription = sndata.short_description;
+    var snStatus = sndata.state; //map to table
+    var snPriority = sndata.priority; //map to table
+    var ticketLink = "https://ecolabsandbox.service-now.com/nav_to.do?uri=incident.do?sys_id=" + sndata.sys_id + "%26sysparm_view=ess" ;
+    //console.log(snNmber);
+
+    //get user details
+    var usersysid = sndata.assigned_to.value;
+    usersysid = usersysid?usersysid:"";
+    //console.log(usersysid);
+
+    if(usersysid != ""){
+      var uoptions = { method: 'GET',
+        url: 'https://ecolabsandbox.service-now.com/api/now/table/sys_user',
+        qs: { sysparm_limit: '1', sys_id: usersysid },
+        headers:
+         {
+           'cache-control': 'no-cache',
+           authorization: 'Bearer LqYAXBdz9pqqPbAKFBhAZpy0g99I0kkmz5I0NJY0czCPDdX0-GrzLYeF9SdAUR46uHL-L64TXkPRlMs8skV7LQ',
+           'content-type': 'application/json',
+           accept: 'application/json' }
+       };
+
+       request(uoptions, function (error, response, body) {
+         if (error) throw new Error(error);
+
+         var ujsonbody = JSON.parse(body);
+         var usndata = ujsonbody.result[0];
+         //console.log(usndata);
+
+         var snAssignedTo = usndata.u_preferred_name;
+         var snRepEmail = usndata.email;
+         var snRepPhone = usndata.phone;
+
+         //console.log(usndata.u_preferred_name);
+
+         var outputString = `Hi! The ticket [${snNmber}](${ticketLink}) - *${snDescription}* has a status of ${snStatus} and a priority of ${snPriority}. \n\n> The ticket is currently assigned to ${snAssignedTo}.  You can contact the rep at ${snRepEmail} or ${snRepPhone}. `;
+
+         bot.say("markdown", outputString);
+         console.log(outputString);
+      }); //user request call
+
+    } //endif
+    else {
+      var outputString = `Hi! The ticket [${snNmber}](${ticketLink}) - *${snDescription}* has a status of ${snStatus} and a priority of ${snPriority}. \n\n > It is currently not assigned yet. `;
+
+      bot.say("markdown", outputString);
+      console.log(outputString);
+    }
+
+  }); //ticket request call
+
+
+});
+
+flint.hears('/sn-opentickets', function(bot, trigger) {
+  console.log("/sn-opentickets fired");
+
+  var userEmail ="";
+  if(trigger.args.length > 1){
+    userEmail =  trigger.args.slice(1).join(" ");
+  }
+  else{
+    userEmail = trigger.personEmail
+  }
+
+  console.log(userEmail);
+
+  var uoptions = { method: 'GET',
+    url: 'https://ecolabsandbox.service-now.com/api/now/table/sys_user',
+    qs: { sysparm_limit: '1',
+          email: userEmail,
+          sysparm_fields: 'sys_id'
+        },
+    headers:
+     {
+       'cache-control': 'no-cache',
+       authorization: 'Bearer LqYAXBdz9pqqPbAKFBhAZpy0g99I0kkmz5I0NJY0czCPDdX0-GrzLYeF9SdAUR46uHL-L64TXkPRlMs8skV7LQ',
+       'content-type': 'application/json',
+       accept: 'application/json' }
+  };
+
+   request(uoptions, function (error, response, body) {
+     if (error) throw new Error(error);
+
+     var ujsonbody = JSON.parse(body);
+     var usndata = ujsonbody.result[0];
+     var usnSysId = usndata.sys_id;
+
+     //query incident table
+     var options = { method: 'GET',
+       url: 'https://ecolabsandbox.service-now.com/api/now/table/incident',
+       qs: { sysparm_limit: '10',
+             sysparm_query: 'state!=7^state!=10',
+             caller_id: usnSysId,
+             sysparm_fields: 'number,sys_id,state,sys_created_by,short_description,priority,active,assignment_group,assigned_to,caller_id'
+           },
+       headers:
+        {
+          'cache-control': 'no-cache',
+          authorization: 'Bearer LqYAXBdz9pqqPbAKFBhAZpy0g99I0kkmz5I0NJY0czCPDdX0-GrzLYeF9SdAUR46uHL-L64TXkPRlMs8skV7LQ',
+          'content-type': 'application/json',
+          accept: 'application/json' }
+      };
+
+     request(options, function (error, response, body) {
+       if (error) throw new Error(error);
+
+       var jsonbody = JSON.parse(body);
+       var sndata = jsonbody.result;
+       //console.log(jsonbody);
+
+       var snNmber = "";
+       var snDescription = "";
+       var ticketLink = "" ;
+       var outputString = "Here are the open tickets you've requested: \n\n ";
+
+      //console.log(jsonbody.result);
+      //console.log(jsonbody.result[0]);
+      //console.log(jsonbody.result[1]);
+
+       for (var t in sndata){
+         console.log(sndata[t]);
+
+         snNmber = sndata[t].number;
+         snDescription = sndata[t].short_description;
+         ticketLink = "https://ecolabsandbox.service-now.com/nav_to.do?uri=incident.do?sys_id=" + sndata[t].sys_id + "%26sysparm_view=ess" ;
+
+           outputString += `[${snNmber}](${ticketLink}) - ${snDescription}  \n`;
+       }
+
+      bot.say("markdown", outputString);
+     }); //end incident request call
+
+  }); //user request call
+
+});
+
+
+/******************************************
+## SPARK APIs
+******************************************/
+
+flint.hears('/room', function(bot, trigger) {
+  console.log("/room fired");
+  let roomId = "*" + trigger.roomId + "*";
+  let roomTitle = "**" + trigger.roomTitle + "**";
+  let outputString = `The roomId for room ${roomTitle} is ${roomId}`;
+  bot.say(outputString);
+});
+
+//// @botname /createroom <room name>
+flint.hears('/createroom', function(bot, trigger) {
+  console.log("/createroom fired");
+  let roomName = trigger.args.slice(1).join(" ");
+
+  //TODO:  how to parse the different emails from the room name
+  let emails = trigger.personEmail;
+  bot.newRoom(roomName, emails);
+
+  //bot.say(bot.personId);
+});
+
+// @botname /dm <email of person to do 1:1>
+//does not work yet
+flint.hears('/dm', function(bot, trigger) {
+  console.log("/dm fired");
+  let dmemail = trigger.args.slice(1).join(" ");
+  bot.dm(dmemail, 'dm test: hello world');
+});
+
+// @botname /rename <room name>
+flint.hears('/renameroom', function(bot, trigger) {
+  let oldroomname = trigger.roomTitle;
+  console.log("/renameroom fired");
+  let roomName = trigger.args.slice(1).join(" ");
+
+  bot.roomRename(roomName)
+  .then(function(err) {
+    console.log(err.message);
+  });
+  if(bot.isModerator)
+  {
+    if(!bot.isGroup){
+      bot.say("Cannot rename a 1:1 or Team room.")
+    }
+    else{
+      let outputString = `This room has been renamed from ${oldroomname} to ${roomName}`
+      bot.say(outputString);
+    }
+  }
+  else{
+    bot.say("I am not a moderator in this room.  I cannot rename the room.")
+  }
+});
+
+//Remove a room and all memberships.
+flint.hears('/implode', function(bot, trigger) {
+  if(!bot.isGroup){
+    bot.say("Cannot delete a 1:1 or Team room.")
+  }
+  else{
+    bot.implode();
+  }
+
+});
 
 /****************************************
 ## Handler for unknown commands
