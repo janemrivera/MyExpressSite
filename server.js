9ab253ee155c8108d70559636b458d173dd3cc22
janemrivera <jane.m.rivera@gmail.com>
2017-02-08 14:36:28 -0600

    check ai again

--- server.js
+++ server.js
@@ -624,6 +624,7 @@ flint.hears(/.*/, function(bot, trigger) {
   console.log("Unknown command fired.");
   bot.say('You see a shimmering light, but it is growing dim...');
 
+
 }, 20);
 
 
@@ -633,13 +634,19 @@ flint.hears(/.*/, function(bot, trigger) {
 // define express path for incoming webhooks
 app.post('/', webhook(flint));
 
-app.post('/ai', (req, res) => {
+app.post('/ai', function(req, res){
+  console.log(req.body.result.action);
+
   if (req.body.result.action === 'weather') {
     let city = req.body.result.parameters['geo-city'];
     //let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+WEATHER_API_KEY+'&q='+city;
-
-    bot.say("weather intent was triggered for %s", city);
+    console.log(city);
+    bot.say(city)
   }
+
+  res.send("testing ai");
+
+  bot.say("weather intent was triggered for %s", city);
 });
 
 //Start up the website
